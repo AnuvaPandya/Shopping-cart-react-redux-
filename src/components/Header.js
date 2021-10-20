d8016111dadc77c86bec28bd24fb59d199c3d883
AnuvaPandya <87873813+AnuvaPandya@users.noreply.github.com>
2021-10-20 13:33:34 +0530

    added redux state

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,7 +1,13 @@
 import React from "react";
 
 const Header = () => {
-  return <div></div>;
+  return (
+    <div className=" ui fixed menu">
+      <div className="ui container center">
+        <h2>Shoppers Point</h2>
+      </div>
+    </div>
+  );
 };
 
 export default Header;
