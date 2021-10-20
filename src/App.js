1c0c389dc4f146690ed46c79aefcb6b6f008a14e
AnuvaPandya <87873813+AnuvaPandya@users.noreply.github.com>
2021-10-20 16:35:37 +0530

    added API

--- src/App.js
+++ src/App.js
@@ -1,10 +1,20 @@
 import "./App.css";
 import Header from "./components/Header";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import ProductDetails from "./components/ProductDetails";
+import ProductListing from "./components/ProductListing";
 
 function App() {
   return (
     <div className="App">
-      <Header />
+      <Router>
+        <Header />
+        <Switch>
+          <Route path="/" exact component={ProductListing} />
+          <Route path="/product/:productId" exact component={ProductDetails} />
+          <Route>404 Not Found</Route>
+        </Switch>
+      </Router>
     </div>
   );
 }
